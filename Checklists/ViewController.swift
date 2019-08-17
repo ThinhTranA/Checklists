fd540609a2beaebe92585672b8a9cbbc809e4fcc
Thinh Tran <thinhtran@outlook.com.au>
2019-08-18 07:14:10 +0800

    Initial Commit

---
+++ Checklists/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Checklists
+//
+//  Created by Thinh on 18/8/19.
+//  Copyright © 2019 Thinh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
