38f55b8c9a676cb5628c8060de617bb1df757df7
Thinh Tran <thinhtran@outlook.com.au>
2019-08-18 09:27:51 +0800

    Add check list

---
+++ Checklists/CheckListViewController.swift
@@ -0,0 +1,110 @@
+//
+//  ViewController.swift
+//  Checklists
+//
+//  Created by Thinh on 18/8/19.
+//  Copyright © 2019 Thinh. All rights reserved.
+//
+
+import UIKit
+
+class CheckListViewController: UITableViewController {
+    let row0text = "Walk the dog"
+    let row1text = "Brush teeth"
+    let row2text = "Learn iOS development"
+    let row3text = "Soccer practice"
+    let row4text = "Eat ice cream"
+    var row0checked = false
+    var row1checked = false
+    var row2checked = false
+    var row3checked = false
+    var row4checked = false
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    // MARK:- Table view data source
+    override func tableView(_ tableView: UITableView,
+                            numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    override func tableView(_ tableView: UITableView,
+                            cellForRowAt indexPath: IndexPath) ->
+        UITableViewCell {
+            let cell = tableView.dequeueReusableCell(
+                withIdentifier: "ChecklistItem",
+                for: indexPath)
+            
+            // Add the following code
+            let label = cell.viewWithTag(1000) as! UILabel
+            if indexPath.row == 0 {
+                label.text = row0text
+            } else if indexPath.row == 1 {
+                label.text = row1text
+            } else if indexPath.row == 2 {
+                label.text = row2text
+            } else if indexPath.row == 3 {
+                label.text = row3text
+            } else if indexPath.row == 4 {
+                label.text = row4text
+            }
+            // End of new code block
+            configureCheckmark(for: cell, at: indexPath)
+            return cell
+    }
+    
+    // MARK:- Table View Delegate
+    override func tableView(_ tableView: UITableView,
+                            didSelectRowAt indexPath: IndexPath) {
+        if let cell = tableView.cellForRow(at: indexPath) {
+            var isChecked = false
+            if indexPath.row == 0 {
+                row0checked = !row0checked
+                isChecked = row0checked
+            } else if indexPath.row == 1 {
+                row1checked = !row1checked
+                isChecked = row1checked
+            } else if indexPath.row == 2 {
+                row2checked = !row2checked
+                isChecked = row2checked
+            } else if indexPath.row == 3 {
+                row3checked = !row3checked
+                isChecked = row3checked
+            } else if indexPath.row == 4 {
+                row4checked = !row4checked
+                isChecked = row4checked
+            }
+            if isChecked {
+                cell.accessoryType = .checkmark
+            } else {
+                cell.accessoryType = .none
+            }
+        }
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    func configureCheckmark(for cell: UITableViewCell,
+                            at indexPath: IndexPath) {
+        var isChecked = false
+        if indexPath.row == 0 {
+            isChecked = row0checked
+        } else if indexPath.row == 1 {
+            isChecked = row1checked
+        } else if indexPath.row == 2 {
+            isChecked = row2checked
+        } else if indexPath.row == 3 {
+            isChecked = row3checked
+        } else if indexPath.row == 4 {
+            isChecked = row4checked
+        }
+        if isChecked {
+            cell.accessoryType = .checkmark
+        } else {
+            cell.accessoryType = .none}
+
+    }
+}
+
